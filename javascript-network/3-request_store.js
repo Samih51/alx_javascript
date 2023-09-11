0b1e16676e8dea1a37624e9f0592928ba5e1e1de
Samih51 <97594964+Samih51@users.noreply.github.com>
2023-09-11 19:44:23 +0300

    Create 3-request_store.js

---
+++ javascript-network/3-request_store.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const req = require('request');
+const fs = require('fs');
+const url = process.argv[2];
+const file = process.argv[3]; 
+
+req.get(url,{encoding: 'utf-8'} , (err, res, body) => {
+    if (err) 
+    console.log(err);
+    else{
+        fs.writeFile(process.argv[3], body);
+    }
+  });
