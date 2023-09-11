593e5792a38b658e01aef98c3c6b7e555f36265e
Samih51 <97594964+Samih51@users.noreply.github.com>
2023-09-11 20:04:06 +0300

    Update 3-request_store.js

--- javascript-network/3-request_store.js
+++ javascript-network/3-request_store.js
@@ -3,12 +3,16 @@
 const req = require('request');
 const fs = require('fs');
 const url = process.argv[2];
-const file = process.argv[3]; 
+const file = process.argv[3];
 
-req.get(url,{encoding: 'utf-8'} , (err, res, body) => {
-    if (err) 
-    console.log(err);
-    else{
-        fs.writeFile(process.argv[3], body, 'utf8');
-    }
-  });
+req.get(url, { encoding: 'utf-8' }, (err, res, body) => {
+  if (err) {
+    console.error(err);
+  } else {
+    fs.writeFile(file, body, 'utf8', (error) => {
+      if (error) {
+        console.log(error);
+      } 
+    });
+  }
+});
