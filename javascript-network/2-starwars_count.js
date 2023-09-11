410ec35e7924943c07ff598a902e4fff44979743
Samih51 <97594964+Samih51@users.noreply.github.com>
2023-09-11 19:20:22 +0300

    Update 2-starwars_count.js

---
+++ javascript-network/2-starwars_count.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+const { log } = require('console');
+const req = require('request');
+const url = process.argv[2];
+let count =0;
+
+req.get(url, (err, res, body) => {
+    if (err) 
+    console.log(err);
+    else{
+        response =JSON.parse(body);
+        let result =response.results;
+        for(let i=0;i<result.length;i++){
+            let character =result[i]['characters']
+            for(let j=0;j<character.length;j++){
+                if (character[j]==='https://swapi-api.alx-tools.com/api/people/18/')
+                    count++;
+            }
+    }
+
+    }
+  });
+  console.log(count);
