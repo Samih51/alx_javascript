f5f329eb2b0601f62d5077d5ebe401bd7b35f834
Samih51 <97594964+Samih51@users.noreply.github.com>
2023-09-11 21:13:06 +0300

    Update 4-completed_tasks.js

--- javascript-network/4-completed_tasks.js
+++ javascript-network/4-completed_tasks.js
@@ -8,13 +8,13 @@ req.get(url, (err, res, body) => {
     if (err) 
     console.log(err);
     else{
-      //  response =JSON.parse(body);
+        response =JSON.parse(body);
 
-        for(let i=0;i<body.length;i++){
+        for(let i=0;i<response.length;i++){
             let count = 0;
             
-            for(let j=0;j<body.length;j++){
-                if (body[j].userId == i && body[j].completed == true)
+            for(let j=0;j<response.length;j++){
+                if (response[j].userId == i && response[j].completed == true)
                     count++;
 
             }
