410e0dc78d56b4ad85b4946632f2d711a9798090
Samih51 <97594964+Samih51@users.noreply.github.com>
2023-09-11 17:52:11 +0300

    Update 1-starwars_title.js

--- javascript-network/1-starwars_title.js
+++ javascript-network/1-starwars_title.js
@@ -5,6 +5,11 @@ const id = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${id}`
 
 req.get(url,{ encoding:'utf-8' }).on('data',function(data){
-    const response= JSON.parse(data);
+   
+    try {
+        const response= JSON.parse(data);
+    } catch (error) {
+        console.error("Error parsing JSON:", error);
+    }
     console.log(response.title);
 });
