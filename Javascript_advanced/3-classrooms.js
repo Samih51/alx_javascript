4f1b3fac80c28fa72dd5823151ee64f997981ad5
Samih51 <97594964+Samih51@users.noreply.github.com>
2023-09-14 16:36:55 +0300

    Update 3-classrooms.js

--- Javascript_advanced/3-classrooms.js
+++ Javascript_advanced/3-classrooms.js
@@ -2,14 +2,16 @@ function createClassRoom(numbersOfStudents){
     function studentSeat(seat){
         return function(){
             return seat;
-        }
+        };
     }
     let students=[];
     for(let i=0;i<=numbersOfStudents;i++){
-        students[i]=i;
+        students.push(studentSeat(i+1));
     }
     return students;
 
 }
 const classRoom = createClassRoom(10);
 console.log(classRoom[0]());
+console.log(classRoom[3]());
+console.log(classRoom[9]());
